14051171bfb16fd1638f82515da075298e5fc90b
ldfracao <ldfracao@protonmail.com>
2022-07-15 23:29:00 -0300

    start settings menu

--- clock.js
+++ clock.js
@@ -2,19 +2,16 @@ let bool = false; //tracks whether the clock is paused or not
 let button = document.getElementById("start_button");
 let time = document.getElementById("time").value;
 let clock = document.getElementsByClassName("clock")[0];
-// time = Number(time);
-clock.innerHTML = time;
+let number = 12;
+
+clock.innerHTML = time + ':' + "0" + number;
 function update_label () {
     if (!time) {
         time = 15;
-        clock.innerHTML = time;
+        clock.innerHTML = (time + ':' + "0" + number).slice(-2);
     } else {
-        // let date = new Date();
-        // let minutes = date.getMinutes();
-        // let seconds = date.getSeconds();
-        // document.getElementsByClassName("clock")[0].innerHTML = `${minutes}:${seconds}`;
-        clock.innerHTML = time;
-        time--;
+        clock.innerHTML = (time + ":" + "0" + number).slice(-2);
+        number--;
     }
 }
 function start_stop (){
@@ -31,17 +28,18 @@ function start_stop (){
 }
 button.onclick = start_stop;
 
-
 // Settings menu
 let settings_button = document.getElementById("settings_button");
-let settings_menu = document.getElementById("settings_menu");
+let settings_menu = document.getElementsByClassName("settings_menu")[0];
 let main = document.getElementById("main");
+let field = document.getElementById("field_size");
+let buyin = document.getElementById("buyin_amount");
 
 settings_menu.style.display = "none";
 function display_menu() {
     if (settings_menu.style.display == "none") {
         main.style.display = "none";
-        settings_menu.style.display = "block";
+        settings_menu.style.display = "flex";
     }
     else{
         settings_menu.style.display = "none";
