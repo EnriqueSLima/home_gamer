6199b5c509db7c387dc691e4cd15255ec58f1bf7
ldfracao <ldfracao@protonmail.com>
2022-08-02 13:10:02 -0300

    Add functions to add and remove players

--- clock.js
+++ clock.js
@@ -5,35 +5,51 @@ let clock = document.getElementById("clock");
 let current_level = document.getElementById("current_level");
 let next_level = document.getElementById("next_level");
 let start_button = document.getElementById("start_button");
-let bool = false; //tracks whether the clock is paused or not
+let player_display = document.getElementById("player_display");
+let add_new_player = document.getElementById("add_new_player");
+let eliminate_player_button = document.getElementById("eliminate_player");
+let general_display = document.getElementsByClassName("general_display");
 
 // SETTINGS MENU VARIABLES
 let settings_menu = document.getElementById("settings_menu");
 let small = document.getElementsByClassName("small_blind");
 let big = document.getElementsByClassName("big_blind");
 let duration = document.getElementsByClassName("time");
+let buyin_money = document.getElementById("buyin_money");
+let buyin_chips = document.getElementById("buyin_chips");
+let rebuy_money = document.getElementById("rebuy_money");
+let rebuy_chips = document.getElementById("rebuy_chips");
+let addon_money = document.getElementById("addon_money");
+let addon_chips = document.getElementById("addon_chips");
+
+// COUNTERS AND AUXILIARY VARIABLES
+let bool = false; //tracks whether the clock is paused or not
 let aux = []; // auxiliary variable used to calculate timer values
 let rounds_counter = 0;
+let player_count = 0;
+let player_left = 0;
 
 // DEFAULT SETTINGS MENU INITIAL VALUES
 for (let index = 0; index < duration.length; index++) {
-  duration[index].value = 15;
   aux[index] = duration[index].value * 60;
 }
 settings_menu.style.display = "none";
 
 // DEFAULT MAIN CONTENT INITIAL VALUES
 round.innerHTML = "Round " + (rounds_counter + 1);
-clock.innerHTML = parseInt(duration[0].value) + ":00";
+clock.innerHTML = duration[0].value + ":00";
 current_level.innerHTML =
-  small[rounds_counter].value + "/" + big[rounds_counter].value;
+  small[rounds_counter].value + " / " + big[rounds_counter].value;
 next_level.innerHTML =
-  small[rounds_counter + 1].value + "/" + big[rounds_counter + 1].value;
+  small[rounds_counter + 1].value + " / " + big[rounds_counter + 1].value;
+general_display[0].innerHTML = "Buy ins ";
+general_display[1].innerHTML = "Rebuys ";
+general_display[2].innerHTML = "Add ons ";
 
 // MAIN MENU FUNCTIONS
 function start_stop() {
   if (!bool) {
-    interval = setInterval(update_timer, 1000);
+    interval = setInterval(update_display, 1000);
     start_button.innerHTML = "Stop";
     bool = true;
   } else {
@@ -42,7 +58,7 @@ function start_stop() {
     bool = false;
   }
 }
-function update_timer() {
+function update_display() {
   if (aux[rounds_counter] == 0) {
     let snd = new Audio("blindsup.mp3");
     snd.play();
@@ -59,43 +75,62 @@ function update_timer() {
 }
 function update_blinds() {
   rounds_counter++;
-  current_level.innerHTML =
-    small[rounds_counter].value + "/" + big[rounds_counter].value;
   round.innerHTML = "Round " + (rounds_counter + 1);
+  current_level.innerHTML =
+    small[rounds_counter].value + " / " + big[rounds_counter].value;
+  next_level.innerHTML =
+    small[rounds_counter + 1].value + " / " + big[rounds_counter + 1].value;
+}
+function add_player() {
+  let player_add = prompt("How many player would you like to add?");
+  let sum = parseInt(player_add, 10);
+  if (sum) {
+    player_count += sum;
+    player_left += sum;
+    player_display.innerHTML = player_left + " / " + player_count;
+  }
+}
+function eliminate_player() {
+  let player_remove = prompt("How many players would you like to remove?");
+  let remove = parseInt(player_remove, 10);
+  if (remove) {
+    player_left -= remove;
+    player_display.innerHTML = player_left + " / " + player_count;
+  }
 }
 
 // SETTINGS MENU FUNCTIONS
 function display_menu() {
   if (settings_menu.style.display == "none") {
     main.style.display = "none";
-    settings_menu.style.display = "grid";
+    settings_menu.style.display = "block";
   } else {
     settings_menu.style.display = "none";
-    main.style.display = "block";
+    main.style.display = "grid";
   }
 }
 
 // MAIN MENU EVENT HANDLERS
 start_button.onclick = start_stop;
 settings_button.onclick = display_menu;
+eliminate_player_button.onclick = eliminate_player;
+add_new_player.onclick = add_player;
 
 // SETTINGS MENU EVENT HANDLERS
 for (let index = 0; index < aux.length; index++) {
   duration[index].onchange = function () {
     aux[index] = this.value * 60;
-    if (rounds_counter == index) {
-      update_timer();
-    }
+    rounds_counter == index ? update_display() : "";
   };
 }
 for (let index = 0; index < small.length; index++) {
   small[index].onchange = function () {
     small[index].value = this.value;
     if (rounds_counter == index) {
-      current_level.innerHTML = small[index].value + "/" + big[index].value;
+      current_level.innerHTML = small[index].value + " / " + big[index].value;
     }
     if (rounds_counter + 1 == index) {
-      next_level.innerHTML = small[index + 1].value + "/" + big[index].value;
+      next_level.innerHTML = small[index].value + " / " + big[index].value;
     }
   };
 }
@@ -103,10 +138,10 @@ for (let index = 0; index < big.length; index++) {
   big[index].onchange = function () {
     big[index].value = this.value;
     if (rounds_counter == index) {
-      current_level.innerHTML = small[index].value + "/" + big[index].value;
+      current_level.innerHTML = small[index].value + " / " + big[index].value;
     }
     if (rounds_counter + 1 == index) {
-      next_level.innerHTML = small[index + 1].value + "/" + big[index].value;
+      next_level.innerHTML = small[index].value + " / " + big[index].value;
     }
   };
 }
